0a0e4a6412e3158f35d2e2d193133b65a54ca561
Oleksandr Hubanov <ohubanov@rocketsoftware.com>
2022-05-02 11:40:08 +0300

    Add project files.

---
+++ MyClass/MyClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bugRecreate.MyClass
+{
+    public class MyClass
+    {
+        public int method() { return 13; }
+    }
+}
